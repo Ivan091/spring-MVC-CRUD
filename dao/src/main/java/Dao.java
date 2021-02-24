bc39f67ecd184d3005fabb74cd3d014be6a40f12
Ivan091 <kornasevich@gmail.com>
2021-02-24 21:30:35 +0300

    Dao interface is made

--- dao/src/main/java/Dao.java
+++ dao/src/main/java/Dao.java
@@ -4,4 +4,12 @@ import java.util.List;
 public interface Dao<T> {
 
     List<T> findAll();
+
+    T findById(int id);
+
+    int update(T entity);
+
+    int create(T entity);
+
+    int delete(int id);
 }
