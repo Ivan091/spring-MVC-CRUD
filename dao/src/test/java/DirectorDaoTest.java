bccf8dab5b9c309b96d4d108b6a8f28579c9ffc3
Ivan091 <kornasevich@gmail.com>
2021-02-25 15:22:52 +0300

    Pom files are made

---
+++ dao/src/test/java/DirectorDaoTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ContextConfiguration;
+
+
+@ContextConfiguration(locations = {"classpath*:test-db.xml", "classpath*:test-dao.xml"})
+class DirectorDaoTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
