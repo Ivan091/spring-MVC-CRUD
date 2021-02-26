52b0dc81277fe02245582b04d200901830f33d62
Ivan091 <kornasevich@gmail.com>
2021-02-26 14:10:29 +0300

    Test can work independent now

--- dao/src/test/java/dao/DirectorDaoTest.java
+++ dao/src/test/java/dao/DirectorDaoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -13,6 +14,7 @@ import java.util.stream.IntStream;
 
 
 @ExtendWith(SpringExtension.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @ContextConfiguration(locations = {"classpath*:test-db.xml", "classpath*:test-dao.xml"})
 class DirectorDaoTest {
 
