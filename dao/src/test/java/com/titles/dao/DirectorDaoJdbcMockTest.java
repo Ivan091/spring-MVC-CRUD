0fa2ae0e69bd96d316cf89c1c1a2bff8c2d653e5
Ivan091 <kornasevich@gmail.com>
2021-03-13 21:08:55 +0300

    Redurant test-db module was merged to dao.

--- dao/src/test/java/com/titles/dao/DirectorDaoJdbcMockTest.java
+++ dao/src/test/java/com/titles/dao/DirectorDaoJdbcMockTest.java
@@ -1,23 +1,14 @@
 package com.titles.dao;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 
-@ExtendWith(MockitoExtension.class)
 public class DirectorDaoJdbcMockTest {
 
-    @InjectMocks
     private DirectorDao directorDao;
 
-    @Mock
     private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
-    @Test
     void test() {
     }
 }
--- test-db/src/main/resources/create-test-db.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS director;
-CREATE TABLE director
-(
-    director_id INT         NOT NULL AUTO_INCREMENT,
-    name        VARCHAR(50) NOT NULL,
-    surname     VARCHAR(50) NOT NULL,
-    birth_date  DATE        NOT NULL,
-    PRIMARY KEY (director_id)
-);
