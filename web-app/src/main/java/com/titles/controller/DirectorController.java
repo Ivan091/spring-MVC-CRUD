9335961cc6257b2f621f5ba3ca07d85a37d336d8
Ivan091 <kornasevich@gmail.com>
2021-03-15 01:20:03 +0300

    Controllers creating started.

--- web-app/src/main/java/com/titles/controller/DirectorController.java
+++ web-app/src/main/java/com/titles/controller/DirectorController.java
@@ -1,5 +1,10 @@
 package com.titles.controller;
 
+import com.titles.model.Director;
+import com.titles.service.ServiceDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +13,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class DirectorController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(RootController.class);
+
+    private final ServiceDao<Director> directorServiceDao;
+
+    public DirectorController(@Qualifier("serviceDirectorDefault") ServiceDao<Director> directorServiceDao) {
+        this.directorServiceDao = directorServiceDao;
+    }
+
     @RequestMapping("/directors")
     public final String directors(Model model) {
-        return "hello";
+
+        return "directors";
+    }
+
+    @RequestMapping("/director")
+    public final String director(Model model) {
+        return "director";
     }
 }
--- web-app/src/main/java/com/titles/controller/RootController.java
+++ web-app/src/main/java/com/titles/controller/RootController.java
@@ -1,34 +1,14 @@
 package com.titles.controller;
 
-import com.titles.model.Director;
-import com.titles.service.ServiceDao;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 public class RootController {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(RootController.class);
-
-    private final ServiceDao<Director> directorServiceDao;
-
-    public RootController(@Qualifier("serviceDirectorDefault") ServiceDao<Director> directorServiceDao) {
-        this.directorServiceDao = directorServiceDao;
-    }
-
     @RequestMapping("/")
-    public final String root(Model model) {
-        model.addAttribute("name", directorServiceDao.findById(1).orElseThrow().toString());
-        return "titles";
-    }
-
-    @RequestMapping("/d")
-    public final String d(Model model) {
-        return "directors";
+    public final String root() {
+        return "redirect:directors";
     }
 }
