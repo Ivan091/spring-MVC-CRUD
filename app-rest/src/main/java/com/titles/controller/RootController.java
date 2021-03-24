9a03df8c95ad8bef67e43a6220299414bebd07b6
Ivan091 <kornasevich@gmail.com>
2021-03-24 23:03:28 +0300

    I started to implement REST.

--- app-rest/src/main/java/com/titles/controller/RootController.java
+++ app-rest/src/main/java/com/titles/controller/RootController.java
@@ -1,10 +1,10 @@
 package com.titles.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class RootController {
 
     @RequestMapping("/")
